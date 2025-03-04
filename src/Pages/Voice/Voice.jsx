619b338f0316ac00dac6927cf741a1746e0a59d5
Ekant Verma <ekantvermasolanki@gmail.com>
2025-03-04 23:17:16 +0530

    optimized

--- src/Pages/Voice/Voice.jsx
+++ src/Pages/Voice/Voice.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ContentImageBlock from '../../CommonComponent/ContentImageBlock';
 import Boxcontainer from '../../CommonComponent/Boxconatainer';
-import {voiceBox} from '../../Constant/Voice';
+import { voiceBox } from '../../Constant/Voice';
 import voice_hero from "../../assets/VoiceImage/lottie/Services-Voice-Hero (1).json";
 
 const sections = [
   {
     component: ContentImageBlock,
     props: {
-      subheading: "Voice Service Provider In India ",
+      subheading: "Voice Service Provider In India",
       heading: "Integrate and tailor voice calls within various applications using the Voice API.",
       content: "Quickly provision and deploy custom voice solutions with flexible APIs on the most extensive global network. Tailor voice solutions to your needs.",
-      secheading:"IVR | Contact Center Solution | Voice OBD | Call Masking",
+      secheading: "IVR | Contact Center Solution | Voice OBD | Call Masking",
       lottie: "use",
       img: voice_hero,
     },
@@ -24,8 +24,6 @@ const sections = [
       carddata: voiceBox,
     },
   },
-  
-
 ];
 
 const Voice = () => {
