d57717bbae5cbe3ad27a6e72502169e444b84906
Ekant Verma <ekantvermasolanki@gmail.com>
2025-04-02 13:15:16 +0530

    port updated

--- backend/src/App.js
+++ backend/src/App.js
@@ -35,11 +35,11 @@ app.use("/api", blogRoutes);
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 connectDB()
   .then(() => {
     console.log("Database Connected successfully!");
-    server.listen(PORT, "0.0.0.0", () => {
+    server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
   });
   })
